c76caac6f7ec34c9372a7447879c6dd334cfbc52
Alex14683 <110792798+alex14683@users.noreply.github.com>
2022-10-27 17:39:24 -0500

    Add Build A React Info Site Project

--- build-a-react-info-site/src/components/Main.js
+++ build-a-react-info-site/src/components/Main.js
@@ -3,8 +3,8 @@ import "../App.css"
 function Main() {
   return (
     <main>
-      <h1 className="mainTitle">Fun facts about React</h1>
-      <ul className="mainFacts">
+      <h1 className="main__title">Fun facts about React</h1>
+      <ul className="main__facts">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100k stars on GitHub</li>
--- build-a-react-info-site/src/components/Navbar.js
+++ build-a-react-info-site/src/components/Navbar.js
@@ -4,9 +4,9 @@ import logo from "../images/react-icon-small.png"
 function Navbar() {
   return (
     <nav>
-      <img src={logo} alt="React logo" className="navIcon" />
-      <h3 className="navLogoText">ReactFacts</h3>
-      <h4 className="navTitle">React Course - Project 1</h4>
+      <img src={logo} alt="React logo" className="nav__icon" />
+      <h3 className="nav__logo-test">ReactFacts</h3>
+      <h4 className="nav__title">React Course - Project 1</h4>
     </nav>
   )
 }

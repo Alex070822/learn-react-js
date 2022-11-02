601f1fec93f6f7020ab8e96106f4f7dffb3575e4
Alex14683 <110792798+alex14683@users.noreply.github.com>
2022-11-02 01:07:46 -0600

    Update image links

--- build-a-react-info-site/src/components/Navbar.js
+++ build-a-react-info-site/src/components/Navbar.js
@@ -1,10 +1,13 @@
 import "../App.css"
-import logo from "../images/react-icon-small.png"
 
 function Navbar() {
   return (
     <nav>
-      <img src={logo} alt="React logo" className="nav__icon" />
+      <img
+        src={require("../images/react-icon-small.png")}
+        alt="React logo"
+        className="nav__icon"
+      />
       <h3 className="nav__logo-test">ReactFacts</h3>
       <h4 className="nav__title">React Course - Project 1</h4>
     </nav>

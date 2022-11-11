2158f7f8695a8a412133a9c936b2ab78e4e99613
Alex14683 <110792798+alex14683@users.noreply.github.com>
2022-11-10 20:23:58 -0600

    Add light/dark mode

--- build-a-react-info-site/src/App.js
+++ build-a-react-info-site/src/App.js
@@ -1,13 +1,20 @@
 import "./App.css"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
+import { useState } from "react"
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true)
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode)
+  }
+
   return (
-    <>
-      <Navbar />
-      <Main />
-    </>
+    <div className="container">
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
+    </div>
   )
 }
 
